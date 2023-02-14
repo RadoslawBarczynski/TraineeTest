70bd3a921d160fd890e2abfc90f956d8ac3a1439
Radosław <teammenpl@gmail.com>
2023-02-14 13:56:07 +0100

    Swapping monsters in army

--- Assets/Scripts/SwapButtonLogic.cs
+++ Assets/Scripts/SwapButtonLogic.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwapButtonLogic : MonoBehaviour
 {
     [SerializeField] private string stringForListDetection = "";
+    [SerializeField] private GameObject Army1Parent, Army2Parent;
 
     //scripts
     private GameManager _gameManager;
@@ -18,40 +21,103 @@ public class SwapButtonLogic : MonoBehaviour
 
     public void SwapMonstersInSelectedBoxes()
     { 
-        int tempIndex = FindIndexInList(_gameManager.selectedBoxesList[0]);
-        int tempIndex2;
-        GameObject tempObject;
-        Vector2 tempVector;
-        //swap indexes 
-        if (stringForListDetection == "army1")
+        int tempIndex = FindIndexInList(_gameManager.selectedBoxesList[0]); //check what army is selected first
+        int tempIndex2; //variable for second selected box
+        GameObject tempObject; //temp object for swaping two objects
+        if (stringForListDetection == "army1")  //------------------logic for army 1-----------------
         {
             tempObject = _gameManager.Army1List[tempIndex];            
             tempIndex2 = FindIndexInList(_gameManager.selectedBoxesList[1]);
             if (stringForListDetection == "army1")
             {
-                _gameManager.Army1List[tempIndex] = _gameManager.Army1List[tempIndex2];               
-                _gameManager.Army1List[tempIndex2] = tempObject;
-                
-                //utworzyæ jakiœ prefab w obiektu jednego dziecka obiektu listy, a nastêpnie wrzuciæ w jego miejsce drugi index, i to samo z pierwszym
-                //po zamianie miejscami jako dzieci wszystko œmiga
+                if(!CheckMonstersToSwap(tempIndex, tempIndex2)) //do when only small monsters are selected
+                {
+                    //change indexes in list of army
+                    _gameManager.Army1List[tempIndex] = _gameManager.Army1List[tempIndex2];
+                    _gameManager.Army1List[tempIndex2] = tempObject;
+
+                    //change gameobject order in list
+                    _gameManager.selectedBoxesList[1].transform.SetSiblingIndex(tempIndex);
+                    _gameManager.selectedBoxesList[0].transform.SetSiblingIndex(tempIndex2);
+                }
+                else //do when Big monster is selected
+                {
+                    SwapFunctionForBigMonster(tempIndex, tempIndex2, tempObject, _gameManager.Army1List);
+                }
             }
             else if(stringForListDetection == "army2")
             {
-                _gameManager.selectedBoxesList[0] = _gameManager.Army2List[tempIndex];
-                _gameManager.selectedBoxesList[1] = tempObject;
+                
             }
         }
-        else if(stringForListDetection == "army2")
+        else if(stringForListDetection == "army2") //------------------logic for army 2-----------------
         {
-            stringForListDetection = "";
-            if (stringForListDetection == "army1")
+            tempObject = _gameManager.Army2List[tempIndex];
+            tempIndex2 = FindIndexInList(_gameManager.selectedBoxesList[1]);
+            if (stringForListDetection == "army2")
+            {
+                if (!CheckMonstersToSwap(tempIndex, tempIndex2)) //do when only small monsters are selected
+                {
+                    //change indexes in list of army
+                    _gameManager.Army2List[tempIndex] = _gameManager.Army2List[tempIndex2];
+                    _gameManager.Army2List[tempIndex2] = tempObject;
+
+                    //change gameobject order in list
+                    _gameManager.selectedBoxesList[1].transform.SetSiblingIndex(tempIndex);
+                    _gameManager.selectedBoxesList[0].transform.SetSiblingIndex(tempIndex2);
+                }
+                else //do when Big monster is selected
+                {
+                    SwapFunctionForBigMonster(tempIndex, tempIndex2, tempObject, _gameManager.Army2List);
+                }
+            }
+            else if (stringForListDetection == "army1")
             {
 
             }
-            else
-            {
+        }
+    }
 
-            }
+    public void SwapFunctionForBigMonster(int index1, int index2, GameObject gameObject, List<GameObject> list)
+    {
+        //swap for selected slot
+        list[index1] = list[index2];
+        list[index2] = gameObject;
+        //swap for second big monster slot
+        GameObject tempIndexForBigMonster = list[index1 + 1]; //new variable for big monster + 1 disabled object
+        list[index1 + 1] = list[index2 + 1];
+        list[index2 + 1] = tempIndexForBigMonster;
+
+        //swap siblings for second big monster slot
+        list[index1 + 1].transform.SetSiblingIndex(index1 + 1);
+        list[index2 + 1].transform.SetSiblingIndex(index2 + 1);
+        //swap selected slots
+        _gameManager.selectedBoxesList[1].transform.SetSiblingIndex(index1);
+        _gameManager.selectedBoxesList[0].transform.SetSiblingIndex(index2);
+    }
+
+
+    public bool CheckMonstersToSwap(int index1, int index2)
+    {
+        if (_gameManager.Army1List[index1].GetComponent<OnClickHandler>().isBigMonsterHere == true || _gameManager.Army1List[index2].GetComponent<OnClickHandler>().isBigMonsterHere == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool CheckIfMonstersCanBeSwaped(int index)
+    {
+        if(_gameManager.Army1List[index].GetComponent<OnClickHandler>().isBigMonsterHere == true && _gameManager.Army1List[index + 1].GetComponent<OnClickHandler>().enabled == false)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
