be5698c45a27d7a2e4783c40ce8cfdadb1085eb2
Radosław <teammenpl@gmail.com>
2023-02-15 20:51:11 +0100

    Swap on first and last index with x1 and x2 monster

--- Assets/Scripts/SwapButtonLogic.cs
+++ Assets/Scripts/SwapButtonLogic.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -24,7 +25,12 @@ public class SwapButtonLogic : MonoBehaviour
         int tempIndex = FindIndexInList(_gameManager.selectedBoxesList[0]); //check what army is selected first
         int tempIndex2; //variable for second selected box
         GameObject tempObject; //temp object for swaping two objects
-        if (stringForListDetection == "army1")  //------------------logic for army 1-----------------
+
+
+        //------------------logic if army 1 is selected first-----------------
+
+
+        if (stringForListDetection == "army1")  
         {
             tempObject = _gameManager.Army1List[tempIndex];            
             tempIndex2 = FindIndexInList(_gameManager.selectedBoxesList[1]);
@@ -47,10 +53,22 @@ public class SwapButtonLogic : MonoBehaviour
             }
             else if(stringForListDetection == "army2")
             {
-                
+                if(!CheckMonstersToSwap(tempIndex, tempIndex2)) //do when only small monsters are selected
+                {
+                    SwapParents(tempIndex, tempIndex2, _gameManager.Army1List, _gameManager.Army2List);
+                }
+                else //do when Big monster is selected
+                {
+                    Debug.Log("for army 1 " + CheckMonstersToSwap(tempIndex, tempIndex2));
+                }
             }
         }
-        else if(stringForListDetection == "army2") //------------------logic for army 2-----------------
+
+
+        //------------------logic if army 2 is selected first-----------------
+
+
+        else if (stringForListDetection == "army2") 
         {
             tempObject = _gameManager.Army2List[tempIndex];
             tempIndex2 = FindIndexInList(_gameManager.selectedBoxesList[1]);
@@ -73,45 +91,146 @@ public class SwapButtonLogic : MonoBehaviour
             }
             else if (stringForListDetection == "army1")
             {
-
+                if (!CheckMonstersToSwap(tempIndex, tempIndex2)) //do when only small monsters are selected
+                {
+                    SwapParents(tempIndex2, tempIndex, _gameManager.Army1List, _gameManager.Army2List);
+                }
+                else //do when Big monster is selected
+                {
+                    Debug.Log(tempIndex);
+                    Debug.Log(tempIndex2);
+                    Debug.Log("for army 2 " + CheckMonstersToSwap(tempIndex, tempIndex2));
+                }
             }
         }
     }
 
+    //------------------------Swap function for one army-------------------------- 
     public void SwapFunctionForBigMonster(int index1, int index2, GameObject gameObject, List<GameObject> list)
     {
-        //swap for selected slot
-        list[index1] = list[index2];
-        list[index2] = gameObject;
-        //swap for second big monster slot
-        GameObject tempIndexForBigMonster = list[index1 + 1]; //new variable for big monster + 1 disabled object
-        list[index1 + 1] = list[index2 + 1];
-        list[index2 + 1] = tempIndexForBigMonster;
+        if (index1 + 1 <= 5 && index2 + 1 <= 5)
+        {
+            //if two elements are big monsters
+            if (list[index1 + 1].GetComponent<OnClickHandler>().enabled == false &&
+                list[index2 + 1].GetComponent<OnClickHandler>().enabled == false ||
+                list[index1 + 1].GetComponent<OnClickHandler>().enabled == true &&
+                list[index2 + 1].GetComponent<OnClickHandler>().enabled == false &&
+                list[index1 + 1].GetComponent<OnClickHandler>().isBigMonsterHere == false ||
+                list[index1 + 1].GetComponent<OnClickHandler>().enabled == false &&
+                list[index2 + 1].GetComponent<OnClickHandler>().enabled == true &&
+                list[index2 + 1].GetComponent<OnClickHandler>().isBigMonsterHere == false)
+            {
+                //-------------if x2 x2   x2 x2 example------------------------------------------------
 
-        //swap siblings for second big monster slot
-        list[index1 + 1].transform.SetSiblingIndex(index1 + 1);
-        list[index2 + 1].transform.SetSiblingIndex(index2 + 1);
-        //swap selected slots
-        _gameManager.selectedBoxesList[1].transform.SetSiblingIndex(index1);
-        _gameManager.selectedBoxesList[0].transform.SetSiblingIndex(index2);
+                //swap for selected slot
+                list[index1] = list[index2];
+                list[index2] = gameObject;
+                //swap for second big monster slot
+
+                GameObject tempIndexForBigMonster = list[index1 + 1]; //new variable for big monster + 1 disabled object
+                list[index1 + 1] = list[index2 + 1];
+                list[index2 + 1] = tempIndexForBigMonster;
+
+                //swap siblings for second big monster slot
+                list[index1 + 1].transform.SetSiblingIndex(index1 + 1);
+                list[index2 + 1].transform.SetSiblingIndex(index2 + 1);
+                //swap selected slots
+                _gameManager.selectedBoxesList[1].transform.SetSiblingIndex(index1);
+                _gameManager.selectedBoxesList[0].transform.SetSiblingIndex(index2);
+            }
+            else if (list[index1 + 1].GetComponent<OnClickHandler>().enabled == true &&
+                list[index2 + 1].GetComponent<OnClickHandler>().enabled == false &&
+                list[index1 + 1].GetComponent<OnClickHandler>().isBigMonsterHere == true)
+            {
+                //-------------if x1 x2 x2 example and x2 is second selection-----------------------
+
+                //swap for selected slot
+                list[index1] = list[index2];
+                list[index2] = gameObject;
+
+                //swap for second big monster slot
+                list[index2] = list[index2 + 1];
+                list[index2 + 1] = gameObject;
+
+                _gameManager.selectedBoxesList[0].transform.SetSiblingIndex(index2 + 1);
+            }
+            else if (list[index1 + 1].GetComponent<OnClickHandler>().enabled == false &&
+                list[index2 + 1].GetComponent<OnClickHandler>().enabled == true &&
+                list[index2 + 1].GetComponent<OnClickHandler>().isBigMonsterHere == true)
+            {
+                //-------------if x1 x2 x2 example and x2 is first selection---------------------
+
+                //swap for selected slot
+                list[index1] = list[index2];
+                list[index2] = gameObject;
+
+                //swap for second big monster slot
+                gameObject = list[index1];
+                list[index1] = list[index1 + 1];
+                list[index1 + 1] = gameObject;
+
+                _gameManager.selectedBoxesList[1].transform.SetSiblingIndex(index1 + 1);
+            }
+        }
+        else
+        {
+        if (list[index1 - 1].GetComponent<OnClickHandler>().enabled == false &&
+            list[index1 - 2].GetComponent<OnClickHandler>().isBigMonsterHere == true)
+            {
+                //-------------if x1 x2 x2 example and x2 is second selection-----------------------
+
+                //swap for selected slot
+                list[index1] = list[index2 + 1];
+                list[index2 + 1] = list[index2];
+                list[index2] = gameObject;
+               
+                _gameManager.selectedBoxesList[0].transform.SetSiblingIndex(index2);
+            }
+        else if(list[index2 - 1].GetComponent<OnClickHandler>().enabled == false &&
+                list[index2 - 2].GetComponent<OnClickHandler>().isBigMonsterHere == true)
+            {
+                //-------------if x1 x2 x2 example and x2 is first selection-----------------------
+                list[index1] = list[index2];
+                list[index2] = list[index1 + 1];
+                list[index1 + 1] = gameObject;
+
+                _gameManager.selectedBoxesList[1].transform.SetSiblingIndex(index1);
+            }
+        }
     }
+
+    //------------------------Swap function for two armys-------------------------- 
+    public void SwapParents(int index1, int index2, List<GameObject> list1, List<GameObject> list2)
+    {
+        //temporary object for slots migration
+        GameObject tempForArmy1 = list1[index1];
+        GameObject tempForArmy2 = list2[index2];
+
+        //swap parents
+        list1[index1].transform.SetParent(Army2Parent.transform);
+        list2[index2].transform.SetParent(Army1Parent.transform);
+
+        //remove duplications
+        list1.Remove(tempForArmy1);
+        list2.Remove(tempForArmy2);
+
+        //insert slots in their correct positions
+        list1.Insert(index1, tempForArmy2);
+        list2.Insert(index2, tempForArmy1);
+
+        //move object to their positions
+        tempForArmy1.transform.SetSiblingIndex(index2);
+        tempForArmy2.transform.SetSiblingIndex(index1);
+    }
+
 
 
     public bool CheckMonstersToSwap(int index1, int index2)
     {
-        if (_gameManager.Army1List[index1].GetComponent<OnClickHandler>().isBigMonsterHere == true || _gameManager.Army1List[index2].GetComponent<OnClickHandler>().isBigMonsterHere == true)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public bool CheckIfMonstersCanBeSwaped(int index)
-    {
-        if(_gameManager.Army1List[index].GetComponent<OnClickHandler>().isBigMonsterHere == true && _gameManager.Army1List[index + 1].GetComponent<OnClickHandler>().enabled == false)
+        if (_gameManager.Army1List[index1].GetComponent<OnClickHandler>().isBigMonsterHere == true ||
+                _gameManager.Army1List[index2].GetComponent<OnClickHandler>().isBigMonsterHere == true ||
+                _gameManager.Army2List[index1].GetComponent<OnClickHandler>().isBigMonsterHere == true ||
+                _gameManager.Army2List[index2].GetComponent<OnClickHandler>().isBigMonsterHere == true)
         {
             return true;
         }
